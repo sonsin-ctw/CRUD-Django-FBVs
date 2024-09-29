21acd429b7ad514fdb1963d808b5665d9b6f29a2
sonsin-ctw <sonsin.choktawee@gmail.com>
2024-09-29 16:12:26 +0700

    Add files via upload

---
+++ crud-django/project/crud/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Item
+from .forms import ItemForm
+
+# Create your views here.
+def item_list(request):
+    items = Item.objects.all()
+    return render(request, 'item_list.html', {'items': items})
+
+def item_detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    return render(request, 'item_detail.html', {'item': item})
+
+def item_create(request):
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('item_list')
+    else:
+        form = ItemForm()
+    return render(request, 'item_form.html', {'form': form})
+
+def item_update(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == "POST":
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('item_list')
+    else:
+        form = ItemForm(instance=item)
+    return render(request, 'item_form.html', {'form': form})
+
+def item_delete(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == "POST":
+        item.delete()
+        return redirect('item_list')
+    return render(request, 'item_confirm_delete.html', {'item': item})
